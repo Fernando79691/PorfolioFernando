48cf52638853d20cdd9da5c909963f6fbf1994eb
Fernando79691 <fernandoantoniocavalcanti@gmail.com>
2025-08-08 09:14:08 -0300

    main

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -2,30 +2,97 @@ import React from 'react'
 import styled from 'styled-components'
 
 const PerfilContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    background-color: var(--cor-backgrount);
+    font-family: var(--font-principal);
 
 `;
 
-const Cards = styled.div`
+const Cabecalho = styled.div`
+    text-align: center;
+    margin-bottom: 50px;
+`;
 
+const Conteudo = styled.div`
+    display: flex;
+    align-items: flex-start;
+    margin-right: 150px;
+    
+    h3 {
+        text-align: center;
+    
+    }
+
+    p {
+        text-align: center;
+
+    }
+
+    button {
+        padding: 10px 20px;
+        height: 60px;
+        width: 150px;
+        font-size: 1rem;
+        background-color: #7c44ec;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-weight: 100;
+        margin-left: 200px;
+    }
+
+
+    /* Responsivo para telas pequenas */
+    @media (max-width: 768px) {
+        flex-direction: column;
+        align-items: stretch;
+    }
+`;
+
+const Desenvolvimento = styled.div`
+    flex: 2;
+    text-align: left;
+`;
+
+const Cards = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+
+    /* Centralizar no mobile */
+    @media (max-width: 768px) {
+        text-align: center;
+    }
 `;
 
 export default function Perfil() {
     return (
-        <>
-            <PerfilContainer>
-                <div>
-                    <h3>Sobre mim</h3>
-                    <p>Aqui você encontrará mais informações, o que estou fazendo, minhas habilidades atuais. </p>
-                </div>
-                <div>
+        <PerfilContainer>
+            <Cabecalho>
+                <h3>Sobre mim</h3>
+                <p>
+                    Aqui você encontrará mais informações, o que estou fazendo, minhas habilidades atuais.
+                </p>
+            </Cabecalho>
+
+            <Conteudo>
+                <Desenvolvimento>
+                    <h3>Conheça</h3>
                     <p>
-                        Transição de carreira para Tecnologia | Desenvolvimento Web
-                        Atualmente focado em Desenvolvimento Front-End com ênfase em JavaScript, React, HTML, CSS e lógica de programação. Mantenho uma rotina de estudos, prática com projetos próprios e atualização constante sobre as tendências da área.
-                        Confira alguns dos meus trabalhos na seção Projetos.
-                        Sinta-se à vontade para se conectar e falar comigo ou me seguir no meu Linkedin e Instagram, onde publico conteúdo útil relacionado a Desenvolvimento Web e Programação.
+                        Transição de carreira para Tecnologia | Desenvolvimento Web.<br />
+                        Atualmente focado em Desenvolvimento Front-End com ênfase <br />em JavaScript, React, HTML, CSS e lógica de programação.<br />
+                        Mantenho uma rotina de estudos, prática com projetos próprios e <br />atualização constante sobre as tendências da área.<br />
+                        Confira alguns dos meus trabalhos na seção Projetos.<br />
+                        Sinta-se à vontade para se conectar e falar comigo<br /> ou me seguir no meu Linkedin e Instagram,
+                        onde publico conteúdo útil<br /> relacionado a Desenvolvimento Web e Programação.
                     </p>
                     <button>Contato</button>
-                </div>
+                </Desenvolvimento>
+
                 <Cards>
                     <h3>Skills</h3>
                     <p>HTML</p>
@@ -36,7 +103,7 @@ export default function Perfil() {
                     <p>Github</p>
                     <p>Mobile First</p>
                 </Cards>
-            </PerfilContainer>
-        </>
-    )
+            </Conteudo>
+        </PerfilContainer>
+    );
 }
